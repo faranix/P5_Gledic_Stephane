7467e79d5ff415e22bd01807f4a695039f8d56e2
Faranix <stephane59119@gmail.com>
2021-01-20 18:16:04 +0100

    Page Product Finis

--- script.js
+++ script.js
@@ -18,11 +18,17 @@ fetch('http://localhost:3000/api/cameras')
           let articleImg = document.getElementsByClassName('article__box__img');
           let articleName = document.querySelectorAll('.article__box__info__name');
           let articleDescription = document.querySelectorAll('.article__box__info__description');
+          let articlePrix = document.querySelectorAll('.article__box__info__achat__prix');
                 
           for(let i = 0; i < articleImg.length; i++){
             articleImg[i].src = urlImg[i].imageUrl;
             articleName[i].innerHTML = urlImg[i].name;
             articleDescription[i].innerHTML = urlImg[i].description;
+          }
+          
+          // Refaire une boucles pour eviter un probleme si l'article n'a pas de prix
+          for(let i = 0; i < articleImg.length; i++) {
+            articlePrix[i].innerHTML = urlImg[i].price + ' Euros';
           }
         }
 
