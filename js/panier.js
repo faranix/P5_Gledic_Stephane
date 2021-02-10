2a91356953251ca550f72b3e06ed66e8081af308
Faranix <stephane59119@gmail.com>
2021-02-10 21:55:00 +0100

    Ajout de la fonction panier

--- js/panier.js
+++ js/panier.js
@@ -1,13 +1,1 @@
-// Recuperer les infos et afficher
-    // Creation des test
-let test1 = document.querySelector('.test');
-let test2 = document.querySelector('.test2');
-
-    // Recuperation des donnees
-test1.innerHTML = localStorage.getItem('nombre-article');
-test2.innerHTML = localStorage.getItem('color-article');
-
-
-
-
-localStorage.setItem('product', Product);
+// Création d'une boucle qui crée le html selon le panier reçu
--- js/product.js
+++ js/product.js
@@ -1,4 +1,3 @@
-let arrayOfProduct = [];
 
 // =============== Parameter in url ============== //
 
@@ -8,7 +7,7 @@ function getIdFromParam(id) {
     const url = window.location.search;   
     if (url == false) {
         console.log("erreur");
-        return false
+        return false;
     } else {
         const urlParams = new URLSearchParams(url)
         id = urlParams.get('id');
@@ -18,35 +17,40 @@ function getIdFromParam(id) {
 
 const id = getIdFromParam();
 
-
-
-
 // Faire un appel a API pour recuperer les data
-async function fetchData() {
-    const response = await fetch('http://localhost:3000/api/cameras/' + id)
-    
-    let data = await response.json();
-    return data;
+
+async function getApi() {
+    const response = await fetch('http://localhost:3000/api/cameras/' + id);
+
+    data = await response.json();
+    // Condition de vérification si les data sont bien charger ou non !
+    if(data != null) {
+        return data;
+    } else {
+        console.log("Pas de data");
+    }
 }
 
-const dataApi = fetchData();    // Stocker les data dans une variable (c'est une promise)
-  
-     
+
+
+
+ const dataApi = getApi();    // Stocker les data dans une variable (c'est une promise)
 
 
 // =============== Reponse de la promesse ============== //
 
 // Utiliser .split pour suprimer (?id=) et garder les nombres dans un tableau
-const idVerification = id.split('=')   
+const idVerification = id.split('='); 
 
 dataApi.then(function htmlCreated(data) {
+    // Ajout d'une condition qui verifie si id existe !
     if (idVerification[0] == data._id) {
         // Creation des variables
         const box = document.querySelector('#box');
         let product = document.createElement('div');
         product.className = 'product';      // Ajout d'une class a product
 
-        // Ecris le code html
+        // Ecris le code html in JS
         product.innerHTML = `
             <img src="${data.imageUrl}" class="product__img">
             <div class="product__article">
@@ -58,7 +62,7 @@ dataApi.then(function htmlCreated(data) {
                 <p class="product__article__description__prix">${data.price} euros</p>
                 
                 <div class="product__article__description__btn">
-                    <form id="form-1" action="panier.html">
+                    <form id="form-1">
                         <select name="nombre" id="nombre">
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -81,7 +85,7 @@ dataApi.then(function htmlCreated(data) {
                             <option value="rouge">Rouge</option>
                         </select>
                     </form>
-                    <button type="submit" value="submit" id="button-panier" onclick="addToCard()"><a href="../pages/panier.html">Mettre au Panier</a></button>
+                    <button type="submit" value="submit" id="button-panier" onclick="addToPanier()"><a href="../pages/panier.html">Mettre au Panier</a></button>
                 </div>
 
             </div>
@@ -89,22 +93,57 @@ dataApi.then(function htmlCreated(data) {
         `
         box.appendChild(product);
     } else {
+        // Si Jamais ID existe pas affiche une erreur !
         let messageErreur = document.createElement('h2');
         messageErreur.innerHTML = "Produit introuvable !"
         box.appendChild(messageErreur);
         console.error(`ID introuvable`);
     }
-}) 
-
+});
 
 // =============== LocalStorage ============== //
 
-// == En cours de creation == // 
- function addToCard() {
-    let numberOfArticle = document.querySelector('#nombre');
-    let colorOfArticle = document.querySelector('#color');
+// Creation d'une fonction addToPanier qui permet d'ajouter des objets dans le localstorage
 
-    localStorage.setItem("color-article", colorOfArticle.value);
-    localStorage.setItem("nombre-article", numberOfArticle.value);
-}
-// ================================== //
+function addToPanier() {
+
+    var panier = {};    // Declaration de la variable en 'var' pour le scope de la variable
+
+    // Recuperation des data saisie par utilisateur 
+    let number = document.querySelector('#nombre');
+    let color = document.querySelector('#color');
+
+    // Appel de la promesse 
+    dataApi.then(() => {
+        if(color == '' || number == '') {
+            console.error('Oups! Vous devez renseigner tout les champs saisie');
+        } else {
+            panier = JSON.parse(localStorage.getItem('panier')) || [];
+            // push du panier avec les data
+            panier.push({
+                image : data.imageUrl,
+                name : data.name,
+                id : data._id,
+                description : data.description,
+                price : data.price,
+                number : number.value,
+                color : color.value
+            })
+
+            // Ajout du panier dans le local storage 
+            window.localStorage.setItem('panier', JSON.stringify(panier));
+            console.log(panier);  
+        }
+    })
+
+};
+
+
+
+
+
+
+
+
+
+
