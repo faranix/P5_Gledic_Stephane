6820e9ba9a51d128b91a963078ef70ec89bac2d7
Faranix <stephane59119@gmail.com>
2021-03-17 18:34:35 +0100

    push for mentor

--- js/panier.js
+++ js/panier.js
@@ -239,6 +239,37 @@ async function postData() {
     });
 }
 
+// ======= EN COURS DE CREATION ======= // 
+/**
+ * Permet de lister les erreurs
+ */
+function showError() {
+    switch (errorMessage) {
+        case 401:
+            console.log("utilisateur non authentifié");
+            break;
+        case 403:
+            console.log("accès refusé");
+            break;
+        case 404:
+            console.log("page non trouvée");
+            break;
+        case 500:
+            console.log("erreur serveur");
+            break;
+        case 503:
+            console.log("erreur serveur");
+            break;
+        case 504:
+            console.log("le serveur n'a pas répondu");
+            break;
+    
+        default:
+            console.log("Une erreur vient d'arriver");
+            break;
+    }
+}
+
 // Event Listener
 let formulaire = document.querySelector('.overlay__box__formulaire');
 
