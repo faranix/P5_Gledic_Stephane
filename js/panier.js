84650d618adad1ccbaa888cb741c8cc7e765eabb
Faranix <stephane59119@gmail.com>
2021-03-23 08:45:48 +0100

    fichier service

--- js/panier.js
+++ js/panier.js
@@ -233,40 +233,10 @@ function postData() {
 
             window.location.href = 'commande.html';
         })
+        .catch(() => {
+            showError(res.status);
+        });
     })
-    .catch(err => {
-        showError(err.status);
-    });
-}
-
-/**
- * Permet de lister les erreurs
- */
-function showError(errorCode) {
-    switch (errorCode) {
-        case 401:
-            console.log("utilisateur non authentifié");
-            break;
-        case 403:
-            console.log("accès refusé");
-            break;
-        case 404:
-            console.log("page non trouvée");
-            break;
-        case 500:
-            console.log("erreur serveur");
-            break;
-        case 503:
-            console.log("erreur serveur");
-            break;
-        case 504:
-            console.log("le serveur n'a pas répondu");
-            break;
-    
-        default:
-            console.log("Une erreur vient d'arriver");
-            break;
-    }
 }
 
 // Event Listener
--- js/product.js
+++ js/product.js
@@ -133,34 +133,3 @@ function addToPanier() {
         localStorage.setItem('panier', JSON.stringify(panier));
     }
 }
-
-/**
- * fonction qui permet de trouvais l'erreur attribuer et envoyer un message en retour
- * @param {*} errorCode 
- */
- function showError(errorCode) {
-    switch (errorCode) {
-        case 401:
-            console.log("utilisateur non authentifié");
-            break;
-        case 403:
-            console.log("accès refusé");
-            break;
-        case 404:
-            console.log("page non trouvée");
-            break;
-        case 500:
-            console.log("erreur serveur");
-            break;
-        case 503:
-            console.log("erreur serveur");
-            break;
-        case 504:
-            console.log("le serveur n'a pas répondu");
-            break;
-    
-        default:
-            console.log("Une erreur vient d'arriver");
-            break;
-    }
-  }
